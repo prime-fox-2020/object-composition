b4a4beb90183efcefa795dee88346db4d395f391
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-04-06 18:42:57 +0700

    done

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 class Cookie {
     constructor(name){
         this.name = name
+        this.form = 'ordinary'
         this.status = 'mentah'
         this.ingredients = []
-        // this.ingredients = this.addIngredient()
+        this.isSugar = this.sugarChecker(name)
     }
 
-    addIngredient(cookieName){
+    addIngredient(cookieName){ //adding ingredients to property
         const ingredient = fs.readFileSync('./ingredients.txt', 'utf-8').split('\n');
 
         ingredient.forEach(el => {
@@ -23,8 +24,31 @@ class Cookie {
         });
     }
 
-    bake(){
-        this.status = 'selesai'
+    bake(){ // changing status
+        if(this.status === 'mentah'){ // if bake 2x it'ill turn overbake
+            this.status = 'selesai'
+        } else {
+            this.status = 'hangus'
+        }
+    }
+
+    sugarChecker(cookieName){
+        const ingredient = fs.readFileSync('./ingredients.txt', 'utf-8').split('\n');
+        let flag = false
+
+        ingredient.forEach(el => {
+            const data = el.split('=');
+            const res = Ingredients.spliter(data[1]);
+
+            if(cookieName === data[0]){
+                for (let i = 0; i < res.length; i++) {
+                    if(res[i][1] === 'sugar' && Number(res[i][0][0]) > 0){
+                        flag = true
+                    }
+                }
+            }
+        });
+        return flag
     }
 }
 class PeanutButter extends Cookie{
@@ -53,10 +77,16 @@ class ChocolateButter extends Cookie{
     }
 }
 class ChocolateChipCrumbled extends ChocolateChip{
-
+    constructor(name, count){
+        super(name, count)
+        this.form = 'crumbled'
+    }
 }
 class PeanutButterCrumbled extends PeanutButter{
-
+    constructor(name, count){
+        super(name, count)
+        this.form = 'crumbled'
+    }
 }
 
 class Ingredients{
@@ -65,7 +95,7 @@ class Ingredients{
         this.amount = amount
     }
 
-    static spliter(data){
+    static spliter(data){ // edit data ingredients from txt
         const ingredient = data.split(', ');
         let amount = [];
 
@@ -79,31 +109,49 @@ class Ingredients{
 }
 
 class CookieFactory{
-    static create(cookies, count, ingredient){
+    static create(cookies, count){
         switch(cookies){
             case 'peanut butter':
-            return new PeanutButter(cookies, count, ingredient);
+            return new PeanutButter(cookies, count);
 
             case 'chocolate chip':
-            return new ChocolateChip(cookies, count, ingredient);
+            return new ChocolateChip(cookies, count);
 
             case 'chocolate cheese':
-            return new ChocolateCheese(cookies, count, ingredient);
+            return new ChocolateCheese(cookies, count);
 
             case 'chocolate butter':
-            return new ChocolateButter(cookies, count, ingredient);
+            return new ChocolateButter(cookies, count);
+
+            case 'chocolate chip crumbled':
+            return new ChocolateChipCrumbled(cookies, count);
+
+            case 'peanut butter crumbled':
+            return new PeanutButterCrumbled(cookies, count);
 
             default:
             return 'Wrong cookie name';
         }
     }
 
-    static cookieRecommendation(day, cookie){
-        
+    static cookieRecommendation(day, instance){
+        switch(day){
+            case 'tuesday':
+            let result = [];
+            instance.forEach(el => {
+                if(!el.isSugar){
+                    result.push(el)
+                }
+            });
+            return result;
+
+            default:
+            break;
+        }
     }
 }
 
-const display = () => {
+let display = () => {
     const options = fs.readFileSync('./cookies.txt', 'utf-8').split('\n');
     const ingredient = fs.readFileSync('./ingredients.txt', 'utf-8').split('\n');
 
@@ -111,83 +159,28 @@ const display = () => {
     let result = [];
 
     ingredient.forEach(el => {
-        const data = el.split('=');
+        let data = el.split('=');
         obj[data[0]] = data[1];
     });
 
-    const peanutButter = new PeanutButter('peanut butter', 100);
-    peanutButter.addIngredient('peanut butter');
-    console.log(peanutButter)
+    options.forEach(cookieName => {
+        let cookie = CookieFactory.create(cookieName, 200);
+        cookie.addIngredient(cookieName);
+        cookie.bake();
+        result.push(cookie)
+        console.log(cookie)
+    });
 
-    const chocolateChip = new ChocolateChip('chocolate chip', 200);
-    chocolateChip.addIngredient('chocolate chip');
-    console.log(chocolateChip)
+    let cookie1 = CookieFactory.create('chocolate chip crumbled', 100);
+    let cookie2 = CookieFactory.create('peanut butter crumbled', 100);
+    console.log(cookie1);
+    console.log(cookie2);
 
-    const chocolateCheese = new ChocolateCheese('chocolate cheese', 250);
-    chocolateCheese.addIngredient('chocolate cheese');
-    console.log(chocolateCheese)
+    let sugarFreeFoods = CookieFactory.cookieRecommendation('tuesday', result);
+    console.log('sugar free foods are :');
 
-    const chocolateButter = new ChocolateButter('chocolate butter', 150);
-    chocolateButter.addIngredient('chocolate butter');
-    console.log(chocolateButter)
-
-    
-    // options.forEach(data => {
-    //     const dataRes = Ingredients.spliter(obj[data]);
-
-    //     switch(data){
-    //         case 'peanut butter':
-    //         const peanut_butter_ing = [];
-    //         dataRes.forEach(el => {
-    //             peanut_butter_ing.push(new Ingredients(el[1], el[0]));
-    //         });
-
-    //         const peanutButter = CookieFactory.create(data, 100, peanut_butter_ing);
-    //         result.push(peanutButter);
-    //         break;
-    
-    //         case 'chocolate chip':
-    //         const choc_chip_ing = [];
-    //         dataRes.forEach(el => {
-    //             choc_chip_ing.push(new Ingredients(el[1], el[0]));
-    //         });
-
-    //         const chocolateChip = CookieFactory.create(data, 200, choc_chip_ing);
-    //         result.push(chocolateChip);
-    //         break;
-    
-    //         case 'chocolate cheese':
-    //         const choc_cheese_ing = [];
-    //         dataRes.forEach(el => {
-    //             choc_cheese_ing.push(new Ingredients(el[1], el[0]));
-    //         });
-
-    //         const chocolateCheese = CookieFactory.create(data, 100, choc_cheese_ing);
-    //         result.push(chocolateCheese);
-    //         break;
-    
-    //         case 'chocolate butter':
-    //         const choc_butter_ing = [];
-    //         dataRes.forEach(el => {
-    //             choc_butter_ing.push(new Ingredients(el[1], el[0]));
-    //         });
-
-    //         const chocolateButter = new ChocolateButter(data, 150, choc_butter_ing);
-    //         result.push(chocolateButter);
-    //         break;
-    
-    //         default:
-    //         break;
-    //     }
-    // });
-
-    result.forEach(instance => {
-        // let sugarFreeFoods = CookieFactory.cookieRecommendation('tuesday', instance);
-        // console.log('sugar free cakes are :');
-        // sugarFreeFoods.forEach(el => {
-        //     console.log(el.name)
-        // });
-        console.log(instance)
+    sugarFreeFoods.forEach(el => {
+        console.log(el.name);
     });
 }
 
