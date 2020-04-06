c85e9609a32abe1a3b4d2f03520a00dbf5c0fa1c
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-04-06 16:15:34 +0700

    Object Composition Done

---
+++ index.js
@@ -0,0 +1,92 @@
+let fs = require('fs')
+let list = fs.readFileSync('cookies.txt', 'utf8').split('\r\n')
+let parsedList = []
+let cookieList = []
+
+class Ingredient {
+    constructor(options){
+        this.name = options[0]
+        this.amount = options[1]
+
+    }
+
+    static parseIngredient(list){ //['peanut butter', [[flour, 1],[sugar, 1],[peanut, 1],[cinnamon, 1],[butter, 1]]]
+        for(let a = 0; a < list.length; a++){
+            let temp1 = list[a].split('=')
+            let temp2 = [temp1[0], temp1[1].split(',')]
+            let temp3 = [temp2[0], []]
+            for(let b = 0; b < temp2[1].length; b++){
+                temp3[1].push(temp2[1][b].split(':'))
+            }
+            parsedList.push(temp3)
+        }
+    }
+}
+
+class Cookie{
+    constructor(ingredient){
+        this.status = 'mentah'
+        this.ingredients = ingredient
+    }
+
+    bake (){
+        this.status = 'selesai dimasak'
+    }
+}
+
+class PeanutButter extends Cookie {
+    constructor(name,ingredient){
+        super(ingredient);
+        this.name = name
+        this.peanut_count = 100
+    }
+}
+
+class ChocolateChip extends Cookie {
+    constructor(name,ingredient){
+        super(ingredient);
+        this.name = name
+        this.choc_chip_count = 200
+    }
+}
+
+class OtherCookie extends Cookie {
+    constructor(name,ingredient){
+        super(ingredient);
+        this.name = name
+        this.other_count = 150
+    }
+}
+
+class CookieFactory {
+    static create(cookies){
+        for(let a = 0; a < cookies.length; a++){
+            switch(cookies[a][0]){
+                case 'peanut butter' :
+                    let temp1 = [] 
+                    for(let b = 0; b < cookies[a][1].length; b++){
+                        temp1.push(new Ingredient(cookies[a][1][b]))
+                    }
+                    cookieList.push(new PeanutButter(cookies[a][0], temp1))
+                break;
+                case 'chocolate chip' :
+                    let temp2 = [] 
+                    for(let b = 0; b < cookies[a][1].length; b++){
+                        temp2.push(new Ingredient(cookies[a][1][b]))
+                    }
+                    cookieList.push(new ChocolateChip(cookies[a][0], temp2))
+                break;
+                default : 
+                    let temp3 = [] 
+                    for(let b = 0; b < cookies[a][1].length; b++){
+                        temp3.push(new Ingredient(cookies[a][1][b]))
+                    }
+                    cookieList.push(new OtherCookie(cookies[a][0], temp3))
+            }
+        }
+    }
+}
+
+Ingredient.parseIngredient(list);
+CookieFactory.create(parsedList)
+console.log(cookieList);
