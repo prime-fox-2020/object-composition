baafa588a7b6804c65f03fd0e751fcdac7e95242
syukur <syukurbaru@gmail.com>
2020-04-06 12:37:31 +0700

    release 0

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 class Cookie {
     constructor() {
         this.status = 'mentah'
+        this.ingredients = []
     }
 
     bake() {
@@ -12,24 +13,44 @@ class Cookie {
 
 class PeanutButter extends Cookie {
     constructor() {
+        super()
         this.peanut_count = 100
     }
 }
 
 class ChocholateChip extends Cookie {
     constructor() {
+        super()
         this.choc_chip_count = 200
     }
 }
 
-let fs = require('fs')
-let options = fs.readFileSync('cookies.txt').toString()
+class OtherCookie extends Cookie {
+    constructor() {
+        super()
+        this.other_count = 150
+    }
+}
+
 
 class CookieFactory {
     static create(cookies) {
+        let resultCookies = []
+        for (let kue of cookies) {
+            resultCookies.push(CookieFactory.searchClass(kue))
+        }
+        return resultCookies
 
     }
+    static searchClass(string) {
+        if (string === 'peanut butter') return new PeanutButter()
+        if (string === 'chocolate chip') return new ChocholateChip()
+        return new OtherCookie()
+    }
 }
+
+let fs = require('fs')
+let options = fs.readFileSync('cookies.txt').toString().split('\n')
 
 let batch_of_cookies = CookieFactory.create(options)
 console.log(batch_of_cookies);
